5ed9e29f7d1b9d4782ead0666bd764cde0d79dc1
impomales <impomales@gmail.com>
2016-06-25 13:03:16 -0400

    bar displays data with axis and labels

--- js/index.js
+++ js/index.js
@@ -2,26 +2,61 @@ $(document).ready(function() {
   var url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
   
   var data = {};
+  var margin = {top: 20, right: 30, bottom: 30, left: 70},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
   
-  function maxGDP(data) {
-    var max = 0;
-    data.forEach(function(d) {
-      if (d[1] > max) max = d[1];
-    });
-    return max;
-  }
+  var x = d3.time.scale()
+    .range([0, width]);
   
+  var y = d3.scale.linear()
+    .range([height, 0]);
+  
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .ticks(14, "%Y");
+  
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, ",f");
+  
+  var chart = d3.select("#chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+   .append("g")
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
   $.getJSON(url, function(data) {
     
-    var x = d3.scale.linear()
-      .domain([0, maxGDP(data.data)])
-      .range([0, 100]);
+    y.domain([0, d3.max(data.data.map(function(d) {return d[1];}))]);
+    x.domain([d3.min(data.data.map(function(d) {return new Date(d[0]);})),
+              d3.max(data.data.map(function(d) {return new Date(d[0]);}))]);
     
-    d3.select("#chart")
-      .selectAll("div")
+    chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+    
+    chart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+     .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .style("font-size", "15px")
+      .text("Gross Domestic Product, USA");
+    
+    chart.selectAll(".bar")
         .data(data.data)
-      .enter().append("div")
-        .style("width", function(d) {return x(d[1]) * 10 + "px";})
-        .text(function(d) {return d[1];})
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) {return x(new Date(d[0]));})
+        .attr("y", function(d) {return y(d[1]);})
+        .attr("height", function(d) {return height - y(d[1]);})
+        .attr("width", width/data.data.length);
   });
 });
